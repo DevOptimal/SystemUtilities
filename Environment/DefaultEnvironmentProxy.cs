b322bfece8a55d6e76711e778bb1fb602175d909
Brad White <bradselw@pm.me>
2022-06-24 12:34:56 -0700

    Fix reference to System namespace

--- Environment/DefaultEnvironmentProxy.cs
+++ Environment/DefaultEnvironmentProxy.cs
@@ -6,12 +6,12 @@ namespace bradselw.System.Resources.Environment
     {
         public string GetEnvironmentVariable(string name, EnvironmentVariableTarget target)
         {
-            return System.Environment.GetEnvironmentVariable(name, target);
+            return global::System.Environment.GetEnvironmentVariable(name, target);
         }
 
         public void SetEnvironmentVariable(string name, string value, EnvironmentVariableTarget target)
         {
-            System.Environment.SetEnvironmentVariable(name, value, target);
+            global::System.Environment.SetEnvironmentVariable(name, value, target);
         }
     }
 }
