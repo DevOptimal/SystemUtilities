4a290fd9b0e3b17a27191b517811e7095406dce4
Brad White <bradselw@pm.me>
2023-05-13 16:00:34 -0700

    Adding some IFileSystem extensions

---
+++ src/FileSystem/Extensions/IFileSystemExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace DevOptimal.SystemUtilities.FileSystem.Extensions
+{
+    public static class IFileSystemExtensions
+    {
+        public static string[] GetDirectories(this IFileSystem fileSystem, string path, bool recursive)
+        {
+            return fileSystem.GetDirectories(path, "*", recursive ? SearchOption.AllDirectories : SearchOption.TopDirectoryOnly);
+        }
+
+        public static string[] GetFiles(this IFileSystem fileSystem, string path, bool recursive)
+        {
+            return fileSystem.GetFiles(path, "*", recursive ? SearchOption.AllDirectories : SearchOption.TopDirectoryOnly);
+        }
+    }
+}
