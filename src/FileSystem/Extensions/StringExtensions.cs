ecd0e5d49858125bc2a9ccd6e19d0dd8144a3aea
Brad White <4261702+bradselw@users.noreply.github.com>
2025-06-25 22:57:55 -0600

    Adding string extensions that operate on file system paths

---
+++ src/FileSystem/Extensions/StringExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+
+namespace DevOptimal.SystemUtilities.FileSystem.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string RemoveInvalidPathChars(this string path) =>
+            ReplaceInvalidPathChars(path, string.Empty);
+
+        public static string ReplaceInvalidPathChars(this string path, char replaceWith) =>
+            ReplaceInvalidPathChars(path, replaceWith.ToString());
+
+        public static string ReplaceInvalidPathChars(this string path, string replaceWith)
+        {
+            if (path == null) throw new ArgumentNullException(nameof(path));
+            char[] invalidChars = Path.GetInvalidPathChars();
+            foreach (char c in invalidChars)
+            {
+                path = path.Replace(c.ToString(), replaceWith);
+            }
+            return path;
+        }
+
+        public static string[] SplitOnDirectorySeparator(this string path)
+        {
+            if (path == null) throw new ArgumentNullException(nameof(path));
+            return path.Split([Path.DirectorySeparatorChar, Path.AltDirectorySeparatorChar], StringSplitOptions.RemoveEmptyEntries);
+        }
+
+        public static string[] SplitOnPathSeparator(this string path)
+        {
+            if (path == null) throw new ArgumentNullException(nameof(path));
+            return path.Split([Path.PathSeparator], StringSplitOptions.RemoveEmptyEntries);
+        }
+    }
+}
---
+++ test/FileSystem.Tests/Extensions/StringExtensionsTests.cs
@@ -0,0 +1,53 @@
+﻿using DevOptimal.SystemUtilities.FileSystem.Extensions;
+
+namespace DevOptimal.SystemUtilities.FileSystem.Tests.Extensions
+{
+    [TestClass]
+    public class StringExtensionsTests
+    {
+        [TestMethod]
+        public void RemoveInvalidPathChars_RemovesInvalidCharacters()
+        {
+            string input = "C:\\foo\r\nbar";
+            string expected = "C:\\foobar";
+            string result = input.RemoveInvalidPathChars();
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void ReplaceInvalidPathChars_ReplacesInvalidCharacters()
+        {
+            string input = "C:\\foo\r\nbar";
+            string expected = "C:\\foo__bar";
+            string result = input.ReplaceInvalidPathChars('_');
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void ReplaceInvalidPathChars_WithString_ReplacesInvalidCharacters()
+        {
+            string input = "C:\\foo\r\nbar";
+            string expected = "C:\\foo----bar";
+            string result = input.ReplaceInvalidPathChars("--");
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void SplitOnDirectorySeparator_SplitsPathCorrectly()
+        {
+            string input = "C:\\foo\\bar\\baz";
+            string[] expected = ["C:", "foo", "bar", "baz"];
+            string[] result = input.SplitOnDirectorySeparator();
+            CollectionAssert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void SplitOnPathSeparator_SplitsPathCorrectly()
+        {
+            string input = "C:\\foo;D:\\bar;E:\\baz";
+            string[] expected = ["C:\\foo", "D:\\bar", "E:\\baz"];
+            string[] result = input.SplitOnPathSeparator();
+            CollectionAssert.AreEqual(expected, result);
+        }
+    }
+}
