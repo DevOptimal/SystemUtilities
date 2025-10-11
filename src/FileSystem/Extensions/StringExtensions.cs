4fcb1e7bfdc381768ac7df898b92f3bf2273e6a3
Brad White <4261702+bradselw@users.noreply.github.com>
2025-10-10 18:34:52 -0600

    Revert "Update extensions with new C# 14 features (#21)"
    This reverts commit 3a88eb55efe88f17d2b91d6c7cb5fe2c0d14f7d9.

--- src/FileSystem/Extensions/StringExtensions.cs
+++ src/FileSystem/Extensions/StringExtensions.cs
@@ -8,83 +8,85 @@ namespace DevOptimal.SystemUtilities.FileSystem.Extensions
     /// </summary>
     public static class StringExtensions
     {
-        extension(string path)
+        /// <summary>
+        /// Removes all invalid path characters from the specified string.
+        /// </summary>
+        /// <param name="path">The input string representing a file system path.</param>
+        /// <returns>
+        /// A new string with all invalid path characters removed.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">
+        /// Thrown if <paramref name="path"/> is <c>null</c>.
+        /// </exception>
+        public static string RemoveInvalidPathChars(this string path) =>
+            ReplaceInvalidPathChars(path, string.Empty);
+
+        /// <summary>
+        /// Replaces all invalid path characters in the specified string with the given character.
+        /// </summary>
+        /// <param name="path">The input string representing a file system path.</param>
+        /// <param name="replaceWith">The character to replace invalid path characters with.</param>
+        /// <returns>
+        /// A new string with all invalid path characters replaced by <paramref name="replaceWith"/>.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">
+        /// Thrown if <paramref name="path"/> is <c>null</c>.
+        /// </exception>
+        public static string ReplaceInvalidPathChars(this string path, char replaceWith) =>
+            ReplaceInvalidPathChars(path, replaceWith.ToString());
+
+        /// <summary>
+        /// Replaces all invalid path characters in the specified string with the given replacement string.
+        /// </summary>
+        /// <param name="path">The input string representing a file system path.</param>
+        /// <param name="replaceWith">The string to replace invalid path characters with.</param>
+        /// <returns>
+        /// A new string with all invalid path characters replaced by <paramref name="replaceWith"/>.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">
+        /// Thrown if <paramref name="path"/> is <c>null</c>.
+        /// </exception>
+        public static string ReplaceInvalidPathChars(this string path, string replaceWith)
         {
-            /// <summary>
-            /// Removes all invalid path characters from the specified string.
-            /// </summary>
-            /// <returns>
-            /// A new string with all invalid path characters removed.
-            /// </returns>
-            /// <exception cref="ArgumentNullException">
-            /// Thrown if <paramref name="path"/> is <c>null</c>.
-            /// </exception>
-            public string RemoveInvalidPathChars() =>
-                ReplaceInvalidPathChars(path, string.Empty);
-
-            /// <summary>
-            /// Replaces all invalid path characters in the specified string with the given character.
-            /// </summary>
-            /// <param name="replaceWith">The character to replace invalid path characters with.</param>
-            /// <returns>
-            /// A new string with all invalid path characters replaced by <paramref name="replaceWith"/>.
-            /// </returns>
-            /// <exception cref="ArgumentNullException">
-            /// Thrown if <paramref name="path"/> is <c>null</c>.
-            /// </exception>
-            public string ReplaceInvalidPathChars(char replaceWith) =>
-                ReplaceInvalidPathChars(path, replaceWith.ToString());
-
-            /// <summary>
-            /// Replaces all invalid path characters in the specified string with the given replacement string.
-            /// </summary>
-            /// <param name="replaceWith">The string to replace invalid path characters with.</param>
-            /// <returns>
-            /// A new string with all invalid path characters replaced by <paramref name="replaceWith"/>.
-            /// </returns>
-            /// <exception cref="ArgumentNullException">
-            /// Thrown if <paramref name="path"/> is <c>null</c>.
-            /// </exception>
-            public string ReplaceInvalidPathChars(string replaceWith)
+            if (path == null) throw new ArgumentNullException(nameof(path));
+            char[] invalidChars = Path.GetInvalidPathChars();
+            foreach (char c in invalidChars)
             {
-                if (path == null) throw new ArgumentNullException(nameof(path));
-                char[] invalidChars = Path.GetInvalidPathChars();
-                foreach (char c in invalidChars)
-                {
-                    path = path.Replace(c.ToString(), replaceWith);
-                }
-                return path;
+                path = path.Replace(c.ToString(), replaceWith);
             }
+            return path;
+        }
 
-            /// <summary>
-            /// Splits the specified path string into segments using directory separator characters (e.g. '/' or '\').
-            /// </summary>
-            /// <returns>
-            /// An array of path segments, split on directory separator characters.
-            /// </returns>
-            /// <exception cref="ArgumentNullException">
-            /// Thrown if <paramref name="path"/> is <c>null</c>.
-            /// </exception>
-            public string[] SplitOnDirectorySeparator()
-            {
-                if (path == null) throw new ArgumentNullException(nameof(path));
-                return path.Split([Path.DirectorySeparatorChar, Path.AltDirectorySeparatorChar], StringSplitOptions.RemoveEmptyEntries);
-            }
+        /// <summary>
+        /// Splits the specified path string into segments using directory separator characters (e.g. '/' or '\').
+        /// </summary>
+        /// <param name="path">The input string representing a file system path.</param>
+        /// <returns>
+        /// An array of path segments, split on directory separator characters.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">
+        /// Thrown if <paramref name="path"/> is <c>null</c>.
+        /// </exception>
+        public static string[] SplitOnDirectorySeparator(this string path)
+        {
+            if (path == null) throw new ArgumentNullException(nameof(path));
+            return path.Split([Path.DirectorySeparatorChar, Path.AltDirectorySeparatorChar], StringSplitOptions.RemoveEmptyEntries);
+        }
 
-            /// <summary>
-            /// Splits the specified path string into segments using the path separator character (e.g. ';').
-            /// </summary>
-            /// <returns>
-            /// An array of path segments, split on the path separator character.
-            /// </returns>
-            /// <exception cref="ArgumentNullException">
-            /// Thrown if <paramref name="path"/> is <c>null</c>.
-            /// </exception>
-            public string[] SplitOnPathSeparator()
-            {
-                if (path == null) throw new ArgumentNullException(nameof(path));
-                return path.Split([Path.PathSeparator], StringSplitOptions.RemoveEmptyEntries);
-            }
+        /// <summary>
+        /// Splits the specified path string into segments using the path separator character (e.g. ';').
+        /// </summary>
+        /// <param name="path">The input string representing a file system path.</param>
+        /// <returns>
+        /// An array of path segments, split on the path separator character.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">
+        /// Thrown if <paramref name="path"/> is <c>null</c>.
+        /// </exception>
+        public static string[] SplitOnPathSeparator(this string path)
+        {
+            if (path == null) throw new ArgumentNullException(nameof(path));
+            return path.Split([Path.PathSeparator], StringSplitOptions.RemoveEmptyEntries);
         }
     }
 }
